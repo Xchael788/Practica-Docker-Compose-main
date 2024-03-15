398cea64a75446e64f3be7bcb606663af145bfca
Xchael788 <evanscuello@gmail.com>
2024-03-15 15:06:26 -0400

    first commit

---
+++ app/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Docker Compose</title>
+</head>
+<body>
+    <h1 >Practica de Docker Compose</h1>
+
+
+<?php
+        echo "<h2>Michael Cuello</h2>";
+        echo "<h2>2022-0219</h2>";
+    ?>
+    
+</body>
+</html>
